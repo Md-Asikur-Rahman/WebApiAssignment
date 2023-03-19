6065e86cab4bd539674f805469f734f6642c2807
Asikur Rahman <asikur.rahman@enosisbd.com>
2023-03-19 11:35:20 +0600

    Created Domain Layer

---
+++ BackEnd/DomainLayer/Data/EmployeeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Data
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Employee> Employees
+
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ BackEnd/DomainLayer/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Enter the employee's FirstName")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Enter the employee's LastName")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Enter the employee's Designation")]
+        [DisplayName("Designation")]
+        public string Designation { get; set; }
+        [Required(ErrorMessage = "Enter the employee's Gender")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Enter the employee's Email")]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter the employee's PhoneNumber")]
+        [Phone]
+        [DisplayName("Phone")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Enter the employee's Description")]
+        [DisplayName("About Me")]
+        public string EmployeeDescription { get; set; }
+        [Required(ErrorMessage = "Enter the employee's Picture")]
+        [DisplayName("Upload Image")]
+        public string EmployeePicuture { get; set; }
+
+    }
+}
